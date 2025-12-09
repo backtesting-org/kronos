578b4bb4a4afdef9435c4004f95ff6761a3a46cc
lwtsn <luke.connor.watson@gmail.com>
2025-12-09 17:40:02 +0700

    chore: expose run strategy command

--- cmd/module.go
+++ cmd/module.go
@@ -1,40 +1,30 @@
 package cmd
 
 import (
-	"os"
-
 	"go.uber.org/fx"
 )
 
 // Module provides all command-related dependencies
-var Module = func() fx.Option {
-	providers := []interface{}{
+var Module = fx.Module("commands",
+	fx.Provide(
 		NewRootCommand,
 		NewInitCommand,
 		//NewLiveCommand,
 		NewBacktestCommand,
 		NewAnalyzeCommand,
 		NewVersionCommand,
+		NewRunStrategyCommand,
 		NewCommands,
-	}
-
-	// Only provide RunStrategyCommand when running run-strategy
-	if len(os.Args) > 1 && os.Args[1] == "run-strategy" {
-		providers = append(providers, NewRunStrategyCommand)
-	}
-
-	return fx.Module("commands",
-		fx.Provide(providers...),
-		fx.Invoke(registerCommands),
-	)
-}()
+	),
+	fx.Invoke(registerCommands),
+)
 
 type registerCommandsParams struct {
 	fx.In
 
 	Root        *RootCommand
 	Cmds        *Commands
-	RunStrategy *RunStrategyCommand `optional:"true"`
+	RunStrategy *RunStrategyCommand
 }
 
 // registerCommands wires up the command tree
@@ -44,9 +34,5 @@ func registerCommands(p registerCommandsParams) {
 	p.Root.Cmd.AddCommand(p.Cmds.Backtest)
 	p.Root.Cmd.AddCommand(p.Cmds.Analyze)
 	p.Root.Cmd.AddCommand(p.Cmds.Version)
-
-	// Only add run-strategy if it was provided
-	if p.RunStrategy != nil {
-		p.Root.Cmd.AddCommand(p.RunStrategy.Cmd)
-	}
+	p.Root.Cmd.AddCommand(p.RunStrategy.Cmd)
 }
